b921cef3ee7b9583cdbd9a1e16836ad4e53c3b42
Arif <aarifkhan873287@gmail.com>
2023-01-16 10:50:36 +0530

    update

--- src/screen/Login/LoginHero.tsx
+++ src/screen/Login/LoginHero.tsx
@@ -4,6 +4,8 @@ import tw from 'twrnc'
 import { Image } from 'react-native'
 
 const LoginHero = ({ navigation }) => {
+    // const type = 'email';
+    const type = 'phone';
     return (
         <View style={[{ height: '100%', width: '100%', backgroundColor: 'black' }, tw`m-auto`]}>
             <StatusBar backgroundColor={'black'} barStyle='default' />
@@ -19,19 +21,24 @@ const LoginHero = ({ navigation }) => {
             <View style={[tw`m-auto`, { width: '90%', height: '35%' }]}>
 
                 <TouchableOpacity
-                onPress={() => navigation.navigate('Signin')}
+                    onPress={() => navigation.navigate('Signin')}
                 >
                     <View style={tw`bg-[#FF6600] rounded-full h-14 my-4`}>
                         <Text style={[tw`text-white text-xl mx-auto my-auto`]}>SIGN IN</Text>
                     </View>
                 </TouchableOpacity>
-                <TouchableOpacity
-                onPress={() => navigation.navigate('Signup')}
-                >
-                    <View style={tw`bg-white rounded-full h-14 my-4`}>
-                        <Text style={[tw`text-black text-xl mx-auto my-auto`]}>SIGN UP</Text>
-                    </View>
-                </TouchableOpacity>
+                {
+                    type == 'email' ?
+                        <TouchableOpacity
+                            onPress={() => navigation.navigate('Signup')}
+                        >
+                            <View style={tw`bg-white rounded-full h-14 my-4`}>
+                                <Text style={[tw`text-black text-xl mx-auto my-auto`]}>SIGN UP</Text>
+                            </View>
+                        </TouchableOpacity>
+                        :
+                        null
+                }
             </View>
         </View>
     )
